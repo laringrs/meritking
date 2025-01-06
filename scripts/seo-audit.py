e1ea6bbc51b588b4a7ae8a065b1b6d2210f5cf42
laringrs <larinaydora@gmail.com>
2025-01-06 22:58:03 +0300

    Update seo-audit.py

--- scripts/seo-audit.py
+++ scripts/seo-audit.py
@@ -4,13 +4,18 @@ from bs4 import BeautifulSoup
 def site_audit(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    title = soup.title.string
-    meta_desc = soup.find("meta", {"name": "description"})
     
+    title = soup.title.string if soup.title else "Title Bulunamadı"
+    meta_desc = soup.find("meta", {"name": "description"})
+    h1_tags = [h1.text for h1 in soup.find_all("h1")]
+    
+    print(f"\nURL: {url}")
     print(f"Site Başlığı: {title}")
-    if meta_desc:
-        print(f"Meta Açıklaması: {meta_desc['content']}")
-    else:
-        print("Meta açıklaması bulunamadı.")
+    print(f"Meta Açıklaması: {meta_desc['content'] if meta_desc else 'Bulunamadı'}")
+    print(f"H1 Etiketleri: {', '.join(h1_tags)}\n")
 
-site_audit("https://meritking.com")
+# Test için site listesi
+site_list = ["https://meritking.com", "https://rakipcasino.com"]
+
+for site in site_list:
+    site_audit(site)
