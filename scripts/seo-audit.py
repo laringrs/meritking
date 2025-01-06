55f4590a0d640a1324c11e9809afa1df6d71a958
laringrs <larinaydora@gmail.com>
2025-01-06 22:46:42 +0300

    Update seo-audit.py

--- scripts/seo-audit.py
+++ scripts/seo-audit.py
@@ -1,1 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
 
+def site_audit(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.title.string
+    meta_desc = soup.find("meta", {"name": "description"})
+    
+    print(f"Site Başlığı: {title}")
+    if meta_desc:
+        print(f"Meta Açıklaması: {meta_desc['content']}")
+    else:
+        print("Meta açıklaması bulunamadı.")
+
+site_audit("https://meritking.com")
